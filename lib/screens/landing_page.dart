99511f07961bd11d78e0b6306a5cc8d69f3d1ebf
John Webi <johnwebi@icloud.com>
2020-01-08 14:27:57 +0300

    initial commit

---
+++ lib/screens/landing_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _LandingPageState();
+  
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     
+    );
+  }
+  
+}
---
+++ lib/styles/ujuzy_theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CinemaAppTheme {
+  CinemaAppTheme._();
+  static const Color nearlyWhite = Color(0xFFFAFAFA);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color background = Color(0xFFF2F3F8);
+  static const Color ujuzyBlue = Color(0xFF4165B0);
+  static const Color ujuzyAccentBlue = Color(0xFFA4466CBE);
+
+  static const Color nearlyBlue = Color(0xFF00B6F0);
+  static const Color nearlyBlack = Color(0xFF213333);
+  static const Color grey = Color(0xFF3A5160);
+  
+  static const String Poppins = 'Poppins';
+  static const String OpenSans = 'OpenSans';
+
+
+}
